42e38e57e986eae74d8d28220ad251356b323acf
Avantika Kesarwani <avikesar2013@gmail.com>
2025-09-21 18:59:07 +0530

    added graph route

--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from . import data, search
+from . import data, search, graph
 
 api_router = APIRouter()
 
 api_router.include_router(search.router, prefix="/api/v1/search", tags=["search"])
 api_router.include_router(data.router, prefix="/api/v1/data", tags=["data"])
+api_router.include_router(graph.router, prefix="/api/v1/graph", tags=["graph"])
